30127deab29d4f649e0e7dfcf18fa9c32d3703d8
kyunghunjung <cliearl@gmail.com>
2022-04-12 20:32:28 +0900

    Practice2A-앱 초기구조 작성하기

--- app/src/main/java/com/qualitybitz/booksearchapp/MainActivity.kt
+++ app/src/main/java/com/qualitybitz/booksearchapp/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.qualitybitz.booksearchapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.qualitybitz.booksearchapp.databinding.ActivityMainBinding
+import com.qualitybitz.booksearchapp.ui.view.FavoriteFragment
+import com.qualitybitz.booksearchapp.ui.view.SearchFragment
+import com.qualitybitz.booksearchapp.ui.view.SettingsFragment
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setupBottomNavigationView()
+        if (savedInstanceState == null) {
+            binding.bottomNavigationView.selectedItemId = R.id.fragment_search
+        }
+    }
+
+    private fun setupBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.fragment_search -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SearchFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_favorite -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, FavoriteFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_settings -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SettingsFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/qualitybitz/booksearchapp/ui/view/FavoriteFragment.kt
@@ -0,0 +1,27 @@
+package com.qualitybitz.booksearchapp.ui.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.qualitybitz.booksearchapp.databinding.FragmentFavoriteBinding
+
+class FavoriteFragment : Fragment() {
+    private var _binding: FragmentFavoriteBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
