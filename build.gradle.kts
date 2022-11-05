31d6edfbe964725713e0cd764d5cee09cade2423
kyunghunjung <cliearl@gmail.com>
2022-11-05 21:34:34 +0900

    Gradle Versions Plugin 도입

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     id(Plugins.SAFEARGS) version Versions.NAVIGATION apply false
     id(Plugins.KOTLIN_SERIALIZATION) version Versions.KOTLIN apply false
     id(Plugins.DAGGER_HILT) version Versions.HILT apply false
+    id("com.github.ben-manes.versions") version "0.43.0"
 }
 
 //task clean(type: Delete) {
@@ -16,3 +17,24 @@ plugins {
 tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
 }
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+tasks.named<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask>("dependencyUpdates")
+    .configure {
+        // isStable에 해당되지 않는 모든 버전을 거부
+        rejectVersionIf {
+            isNonStable(candidate.version)
+        }
+
+        // optional parameters
+        checkForGradleUpdate = true
+        outputFormatter = "json"
+        outputDir = "build/dependencyUpdates"
+        reportfileName = "report"
+    }
